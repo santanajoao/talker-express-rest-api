7e5a040e6ce6ee1a5576ba75f959dce722c09ed6
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-16 09:53:39 -0300

    corrige erros de lint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const talkerRouter = require('./routes/talkerRouter');
 const app = express();
 app.use(express.json());
 
-app.use('/talker', talkerRouter)
+app.use('/talker', talkerRouter);
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
--- src/utils/talker.js
+++ src/utils/talker.js
@@ -3,10 +3,10 @@ const fs = require('fs').promises;
 
 const FILE_PATH = path.resolve(__dirname, '../talker.json');
 
-function JSONfy(data) {
-  const INDENTATION = 2;
-  return JSON.stringify(data, null, INDENTATION);
-}
+// function JSONfy(data) {
+//   const INDENTATION = 2;
+//   return JSON.stringify(data, null, INDENTATION);
+// }
 
 async function getTalkers() {
   const response = await fs.readFile(FILE_PATH);
