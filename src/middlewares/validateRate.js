f64639590afea9937e15854244446fcce1d2dcdc
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-16 11:07:03 -0300

    corrige erros de lint

--- src/middlewares/validateRate.js
+++ src/middlewares/validateRate.js
@@ -1,16 +1,16 @@
-function validateRate(req, res, next) {
+function validateRate(req, _res, next) {
   const BAD_REQUEST = 400;
   const MIN_RATE = 1;
   const MAX_RATE = 5;
   const { rate } = req.body.talk;
-  if (rate === undefined || rate === null) {
+  if (rate === undefined) {
     return next({
       status: BAD_REQUEST, message: 'O campo "rate" é obrigatório',
     });
   }
 
-  const isInteger = parseInt(rate, 10) === rate;
-  if (rate < MIN_RATE || rate > MAX_RATE || !isInteger) {
+  const notInteger = parseInt(rate, 10) !== rate;
+  if (rate < MIN_RATE || rate > MAX_RATE || notInteger) {
     return next({
       status: BAD_REQUEST,
       message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
